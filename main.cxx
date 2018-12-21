192c2a5980bb3be54d33156a3df7f7e98af87fa5
Daria Wiśniowska <daria.wisniowska123@gmail.com>
2018-12-21 21:37:14 +0100

    Ograniczenie ilości okręgów

--- main.cxx
+++ main.cxx
@@ -30,6 +30,7 @@
 #include "itkLaplacianImageFilter.h"
 
 #include "itkSliceBySliceImageFilter.h"
+#include "itkEllipseSpatialObject.h"
 
 std::string path = "Images/Input/Gsp9_Gsp9/Head_Routine - 20091228/";
 
@@ -37,25 +38,20 @@ std::string path = "Images/Input/Gsp9_Gsp9/Head_Routine - 20091228/";
 /*Pixel Types*/
 typedef float PixelType;
 typedef float AccumulatorPixelType;
-typedef float RadiusPixelType;
-typedef float PixelType3D;
 typedef float OutputPixelType;
 
 /*Dimensions*/
 const unsigned int Dimension = 2;
-const int Dimension3D = 3;
 
 /*Image Types*/
 typedef itk::Image <PixelType, Dimension > ImageType;
-typedef itk::Image <PixelType3D, Dimension3D> ImageType3D;
-typedef itk::Image <PixelType3D, Dimension3D> OutImageType3D;
 typedef itk::Image <AccumulatorPixelType, Dimension > AccumulatorImageType;
 typedef itk::Image <OutputPixelType, Dimension > OutputImageType;
 
 /*Hough Transform Consts*/
 constexpr int numberOfCircles = 5;
-constexpr float radiusMin = 2.1;
-constexpr float radiusMax = 22.4;
+constexpr float radiusMin = 1.1; //mm
+constexpr float radiusMax = 22.1; //mm
 constexpr float radiusAvg = 16.2;
 constexpr float sweepAngle = 0;
 constexpr float sigmaGradient = 1.2;
@@ -110,10 +106,12 @@ HoughTransformFilterType::CirclesListType HoughTransform(ImageType::Pointer imag
 
 int main()
 {
-	std::string directory = "C:\\POMwJO\\ITK-projekt\\Images\\Input\\mozg md\\Head_Neck_Standard - 1\\t1_vibe_fs_hi_res_20";
+	std::string directory = "C:\\POMwJO\\ITK-projekt\\Images\\Input\\Gsp13_Gsp13\\Head_Routine - 20090915\\T1_SE_TRA_PAT2_4";
+	//std::string directory = "C:\\POMwJO\\ITK-projekt\\Images\\Input\\mozg zb\\Head_Neck_Standard - 153275\\t1_vibe_fs_cor_CM_19";
 	itk::GDCMSeriesFileNames::Pointer namesGenerator;
 	namesGenerator = itk::GDCMSeriesFileNames::New();
 	namesGenerator->SetDirectory(directory);
+
 	itk::SerieUIDContainer series = namesGenerator->GetSeriesUIDs();
 	for (size_t i = 0; i < series.size(); i++)
 	{
@@ -124,9 +122,143 @@ int main()
 		namesSeriesGenerator->SetStartIndex(1);
 		namesSeriesGenerator->SetEndIndex(filenames.size());
 		itk::FilenamesContainer outputFilenames = namesSeriesGenerator->GetFileNames();
+
+		std::vector<HoughTransformFilterType::CirclesListType> circles(filenames.size());
+		std::vector<HoughTransformFilterType::CirclesListType> savedCircles(filenames.size());
+		std::vector<HoughTransformFilterType::CirclesListType> finalCircles(filenames.size());
+
+
 		for (size_t k = 0; k < filenames.size(); k++)
 		{
-			
+
+			typedef itk::ImageFileReader< ImageType > ReaderType;
+			ReaderType::Pointer reader = ReaderType::New();
+			reader->SetFileName(filenames[k]);
+			try
+			{
+				reader->Update();
+				std::cout << "- - - - - - - - - - - - - - \n";
+				std::cout << "Read Image: " << filenames[k] << std::endl;
+				std::cout << "- - - - - - - - - - - - - - \n";
+			}
+			catch (itk::ExceptionObject & excep)
+			{
+				std::cerr << "Exception caught !" << std::endl;
+				std::cerr << excep << std::endl;
+				return EXIT_FAILURE;
+			}
+			ImageType::Pointer localImage = reader->GetOutput();
+
+			circles[k] = HoughTransform(localImage);
+			std::cout << "		Found " <<circles[k].size()<<" circles. "<< std::endl;
+		}
+	
+		std::cout << "- - - - - - - - - - - - - - \n";
+		std::cout << "Validation" << std::endl;
+		std::cout << "- - - - - - - - - - - - - - \n";
+		typedef HoughTransformFilterType::CirclesListType CirclesListType;
+		savedCircles = circles;
+		for (size_t j = 0; j < circles.size() ; j++)
+		{
+			CirclesListType::const_iterator itCircles = circles[j].begin();
+			while (itCircles != circles[j].end())
+			{
+				if (j >= 2 && j < circles.size() - 2)
+				{
+					//pêtla po preprevious
+					bool preprevious = false;
+					size_t prog = 10;
+					CirclesListType::const_iterator itCirclesPrePrevious = circles[j - 2].begin();
+					while (itCirclesPrePrevious != circles[j - 2].end())
+					{
+						if ((((*itCircles)->GetObjectToParentTransform()->GetOffset()[0] >= (*itCirclesPrePrevious)->GetObjectToParentTransform()->GetOffset()[0] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[0] <= (*itCirclesPrePrevious)->GetObjectToParentTransform()->GetOffset()[0] + prog)
+							&& ((*itCircles)->GetObjectToParentTransform()->GetOffset()[1] >= (*itCirclesPrePrevious)->GetObjectToParentTransform()->GetOffset()[1] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[1] <= (*itCirclesPrePrevious)->GetObjectToParentTransform()->GetOffset()[1] + prog)))
+						{
+
+							preprevious = true;
+
+						}
+						itCirclesPrePrevious++;
+					}
+					//pêtla po previous
+					bool previous = false;
+					CirclesListType::const_iterator itCirclesPrevious = circles[j - 1].begin();
+					while (itCirclesPrevious != circles[j - 1].end())
+					{
+						if ((((*itCircles)->GetObjectToParentTransform()->GetOffset()[0] >= (*itCirclesPrevious)->GetObjectToParentTransform()->GetOffset()[0] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[0] <= (*itCirclesPrevious)->GetObjectToParentTransform()->GetOffset()[0] + prog)
+							&& ((*itCircles)->GetObjectToParentTransform()->GetOffset()[1] >= (*itCirclesPrevious)->GetObjectToParentTransform()->GetOffset()[1] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[1] <= (*itCirclesPrevious)->GetObjectToParentTransform()->GetOffset()[1] + prog)))
+						{
+
+							previous = true;
+
+						}
+						itCirclesPrevious++;
+					}
+					//pêtle po next
+					bool next = false;
+					CirclesListType::const_iterator itCirclesNext = circles[j + 1].begin();
+					while (itCirclesNext != circles[j + 1].end())
+					{
+						if ((((*itCircles)->GetObjectToParentTransform()->GetOffset()[0] >= (*itCirclesNext)->GetObjectToParentTransform()->GetOffset()[0] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[0] <= (*itCirclesNext)->GetObjectToParentTransform()->GetOffset()[0] + prog)
+							&& ((*itCircles)->GetObjectToParentTransform()->GetOffset()[1] >= (*itCirclesNext)->GetObjectToParentTransform()->GetOffset()[1] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[1] <= (*itCirclesNext)->GetObjectToParentTransform()->GetOffset()[1] + prog)))
+						{
+
+							next = true;
+
+						}
+						itCirclesNext++;
+					}
+					//pêtle po next
+					bool postnext = false;
+					CirclesListType::const_iterator itCirclesPostNext = circles[j + 2].begin();
+					while (itCirclesPostNext != circles[j + 2].end())
+					{
+						if ((((*itCircles)->GetObjectToParentTransform()->GetOffset()[0] >= (*itCirclesPostNext)->GetObjectToParentTransform()->GetOffset()[0] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[0] <= (*itCirclesPostNext)->GetObjectToParentTransform()->GetOffset()[0] + prog)
+							&& ((*itCircles)->GetObjectToParentTransform()->GetOffset()[1] >= (*itCirclesPostNext)->GetObjectToParentTransform()->GetOffset()[1] - prog && (*itCircles)->GetObjectToParentTransform()->GetOffset()[1] <= (*itCirclesPostNext)->GetObjectToParentTransform()->GetOffset()[1] + prog)))
+						{
+
+							postnext = true;
+
+						}
+						itCirclesPostNext++;
+					}
+					//if to albo to true zostaje 
+					if (previous && next)
+					{
+						if (previous && preprevious)
+						{
+							//std::cout << "saved" << std::endl;
+						}
+						else if (next && postnext)
+						{
+							//std::cout << "saved" << std::endl;
+						}
+						else
+						{
+							//std::cout << "popped" << std::endl;
+							savedCircles[j].remove((*itCircles));
+						}
+
+					}
+					else
+					{
+						//std::cout << "popped" << std::endl;
+						savedCircles[j].remove((*itCircles));
+					}
+				}
+				else
+					savedCircles[j].remove((*itCircles));
+				
+				itCircles++;
+			}
+
+			finalCircles[j] = savedCircles[j];
+		}
+
+
+		for (size_t k = 0; k < filenames.size(); k++)
+		{
+
 			typedef itk::ImageFileReader< ImageType > ReaderType;
 			ReaderType::Pointer reader = ReaderType::New();
 			reader->SetFileName(filenames[k]);
@@ -153,15 +285,16 @@ int main()
 			localOutputImage->SetOrigin(localImage->GetOrigin());
 			localOutputImage->SetSpacing(localImage->GetSpacing());
 			localOutputImage->Allocate(true);
-
-			HoughTransformFilterType::CirclesListType circles = HoughTransform(localImage);
+			
+			circles[k] = finalCircles[k];
+			std::cout << "		Saved " << circles[k].size() << " circles. " << std::endl;
 
 			typedef HoughTransformFilterType::CirclesListType CirclesListType;
-			CirclesListType::const_iterator itCircles = circles.begin();
-			while (itCircles != circles.end())
+			CirclesListType::const_iterator itCircles = circles[k].begin();
+			while (itCircles != circles[k].end())
 			{
-				std::cout << "Center: " << (*itCircles)->GetObjectToParentTransform()->GetOffset() << std::endl;
-				std::cout << "Radius: " << (*itCircles)->GetRadius()[0] << std::endl;
+				//std::cout << "Center: " << (*itCircles)->GetObjectToParentTransform()->GetOffset() << std::endl;
+				//std::cout << "Radius: " << (*itCircles)->GetRadius()[0] << std::endl;
 				for (double angle = 0;
 					angle <= itk::Math::twopi;
 					angle += itk::Math::pi / 60.0)
@@ -195,6 +328,7 @@ int main()
 			try
 			{
 				writer->Update();
+				std::cout << "		Overwrited image" << std::endl;
 			}
 			catch (itk::ExceptionObject & excep)
 			{
@@ -203,7 +337,9 @@ int main()
 				return EXIT_FAILURE;
 			}
 		}
+
 	}
+
 	
 	
 
